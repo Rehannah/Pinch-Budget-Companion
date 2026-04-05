e96bd3a304f371e3a83716643701dc5087c673d5
Rehannah <rehannah12345@gmail.com>
2026-04-05 04:10:53 -0400

    gh pages conflict resolved

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+	base: "/Pinch-Budget-Companion/", // ← add this
 	root: "src",
 	build: {
 		outDir: "../dist",
